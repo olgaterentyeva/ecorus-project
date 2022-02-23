8834fd3cc3a8e25fb50b5ffa1f85a4b4fbdcf695
terentyevaolga <terentyeevaolga@gmail.com>
2022-02-23 18:14:17 +0300

    added slider

--- src/components/Swiper/Slider.tsx
+++ src/components/Swiper/Slider.tsx
@@ -1,7 +1,7 @@
 import {Swiper, SwiperSlide} from 'swiper/react';
 import SwiperCore, {Navigation} from 'swiper';
 import 'swiper/scss';
-import 'swiper/scss/navigation';
+// import 'swiper/scss/navigation';
 import {Card} from "../Card/Card";
 import serviceCondition from "../../svg-icons/serviceCondition.svg";
 import plastic from "../../svg-icons/plastic.svg";
@@ -10,11 +10,11 @@ import masks from "../../svg-icons/masks.svg";
 
 export const Slider = () => {
 
-    SwiperCore.use([Navigation]);
+    // SwiperCore.use([Navigation]);
 
     return (
         <Swiper
-            navigation
+            // navigation
             slidesPerView={1}
             onSlideChange={() => console.log('slide change')}
             onSwiper={(swiper) => console.log(swiper)}
@@ -24,23 +24,30 @@ export const Slider = () => {
                     cardHeadline="Сделаем мир чище"
                     cardText="Сдай макулатуру или старую одежду и получи скидку на покупку товаров из переработанных материалов"
                     type="button"
+                    buttonText = "Условия сервиса"
                     img={serviceCondition}
+                    bgColor={"#B3EDC8"}
                 />
             </SwiperSlide>
             <SwiperSlide>
                 <Card
-                    cardHeadline="Сделаем мир чище"
-                    cardText="Сдай макулатуру или старую одежду и получи скидку на покупку товаров из переработанных материалов"
+                    cardHeadline="А вы знали...."
+                    cardText="что среднее время разложения пластмассовых изделий колеблется от 400 до 700 лет,  а полиэтиленовых пакетов — от 100 до 200 лет?"
                     type="button"
+                    buttonText = "Узнать больше"
                     img={plastic}
+                    bgColor={"#FFE48F"}
+
                 />
             </SwiperSlide>
             <SwiperSlide>
                 <Card
-                    cardHeadline="Сделаем мир чище"
-                    cardText="Сдай макулатуру или старую одежду и получи скидку на покупку товаров из переработанных материалов"
+                    cardHeadline="Что с масками?"
+                    cardText="Медицинские маски не обязательно должны становиться отходами. Их тоже можно сдать на переработку."
                     type="button"
+                    buttonText = "Пункты сбора масок"
                     img={masks}
+                    bgColor={"#BFF0DE"}
                 />
             </SwiperSlide>
         </Swiper>
