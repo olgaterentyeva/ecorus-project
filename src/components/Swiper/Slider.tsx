9480656fa602645cb97d21ec3c5099fe94f15a80
Liaisan Akhmetova <akhmetova1423@gmail.com>
2022-02-24 13:23:06 +0300

    slider arrow outside the slider

--- src/components/Swiper/Slider.tsx
+++ src/components/Swiper/Slider.tsx
@@ -8,49 +8,61 @@ import serviceCondition from "../../svg-icons/serviceCondition.svg";
 import plastic from "../../svg-icons/plastic.svg";
 import masks from "../../svg-icons/masks.svg";
 
-
 export const Slider = () => {
 
     SwiperCore.use([Navigation]);
 
     return (
-        <Swiper
-            navigation
-            slidesPerView={1}
-            onSlideChange={() => console.log('slide change')}
-            onSwiper={(swiper) => console.log(swiper)}
-        >
-            <SwiperSlide>
-                <Card
-                    cardHeadline="Сделаем мир чище"
-                    cardText="Сдай макулатуру или старую одежду и получи скидку на покупку товаров из переработанных материалов"
-                    type="button"
-                    buttonText="Условия сервиса"
-                    img={serviceCondition}
-                    bgColor={"#B3EDC8"}
-                />
-            </SwiperSlide>
-            <SwiperSlide>
-                <Card
-                    cardHeadline="А вы знали...."
-                    cardText="что среднее время разложения пластмассовых изделий колеблется от 400 до 700 лет,  а полиэтиленовых пакетов — от 100 до 200 лет?"
-                    type="button"
-                    buttonText="Узнать больше"
-                    img={plastic}
-                    bgColor={"#FFE48F"}
+        <>
+            <div className="slider">
+                <div className = "navigation">
+                    <div className="swiper-button-prev-custom">
+                    </div>
+                    <div className="swiper-button-next-custom">
+                    </div>
+                </div>
 
-                />
-            </SwiperSlide>
-            <SwiperSlide>
-                <Card
-                    cardHeadline="Что с масками?"
-                    cardText="Медицинские маски не обязательно должны становиться отходами. Их тоже можно сдать на переработку."
-                    type="button"
-                    buttonText="Пункты сбора масок"
-                    img={masks}
-                    bgColor={"#BFF0DE"}
-                />
-            </SwiperSlide>
-        </Swiper>
-    );
+                <Swiper
+                    navigation
+                    slidesPerView={1}
+                    onSlideChange={() => console.log('slide change')}
+                    onSwiper={(swiper) => console.log(swiper)}
+                >
+                    <SwiperSlide>
+                        <Card
+                            cardHeadline="Сделаем мир чище"
+                            cardText="Сдай макулатуру или старую одежду и получи скидку на покупку товаров из переработанных материалов"
+                            type="button"
+                            buttonText="Условия сервиса"
+                            img={serviceCondition}
+                            bgColor={"#B3EDC8"}
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Card
+                            cardHeadline="А вы знали...."
+                            cardText="что среднее время разложения пластмассовых изделий колеблется от 400 до 700 лет,  а полиэтиленовых пакетов — от 100 до 200 лет?"
+                            type="button"
+                            buttonText="Узнать больше"
+                            img={plastic}
+                            bgColor={"#FFE48F"}
+
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Card
+                            cardHeadline="Что с масками?"
+                            cardText="Медицинские маски не обязательно должны становиться отходами. Их тоже можно сдать на переработку."
+                            type="button"
+                            buttonText="Пункты сбора масок"
+                            img={masks}
+                            bgColor={"#BFF0DE"}
+                        />
+                    </SwiperSlide>
+                </Swiper>
+
+            </div>
+        </>
+    )
+        ;
 }
