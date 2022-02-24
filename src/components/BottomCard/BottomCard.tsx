e9dd4980d2a78a77a105b0465b4f9fb4e0b1e166
Liaisan Akhmetova <akhmetova1423@gmail.com>
2022-02-24 15:22:30 +0300

    button fixed without styling

--- src/components/BottomCard/BottomCard.tsx
+++ src/components/BottomCard/BottomCard.tsx
@@ -3,31 +3,36 @@ import {Button} from "../ui/Button/Button";
 import {Icon} from "../../icon/Icon";
 import styles from './BottomCard.module.scss'
 
-interface BottomCardProps{
+interface BottomCardProps {
     cardHeadline: string,
-    cardText:string,
-    type:string,
-    img:any
+    cardText: string,
+    type: string,
+    img: any
 }
 
-export const BottomCard:FC<BottomCardProps> = (props) => {
+export const BottomCard: FC<BottomCardProps> = (props) => {
 
     const onBottomButtonClick = () => {
-      console.log("click")
+        console.log("click")
     }
 
-  return(
-      <>
-          <div className={styles.informBlock}>
-              <div className={styles.informBlock__info}>
-                  <h1>{props.cardHeadline}</h1>
-                  <p>{props.cardText}</p>
-                  <Button type={props.type} onClick={onBottomButtonClick} className = {styles.cardButton}>
-                      <Icon name = "buttonArrow" width="24" height="24"/>
-                  </Button>
-              </div>
-              <img src={props.img} alt="arrow"/>
-          </div>
-      </>
-  )
+    return (
+        <>
+            <div className={styles.informBlock}>
+                <div className={styles.informBlock__info}>
+                    <h1>{props.cardHeadline}</h1>
+                    <p>{props.cardText}</p>
+                    <div className = {styles.cardButton}>
+                        <Button
+                            type={props.type}
+                            onClick={onBottomButtonClick}
+                            buttonColor={"rgba(0, 0, 0, 0.12)"}
+                            buttonImg={<Icon name="buttonArrow" width="24" height="24"/>}
+                        />
+                    </div>
+                </div>
+                <img src={props.img} alt="arrow"/>
+            </div>
+        </>
+    )
 }
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -23,7 +23,7 @@ export const Card:FC<CardProps> = (props) => {
               <div className={styles.section__info}>
                   <h1>{props.cardHeadline}</h1>
                   <p>{props.cardText}</p>
-                  <Button type={props.type} onClick={onServiceConditionClick} buttonText={props.buttonText}/>
+                  <Button type={props.type} onClick={onServiceConditionClick} buttonText={props.buttonText} buttonColor={"#07C88E"} />
               </div>
               <img src={props.img} alt="serviceCondition"/>
           </div>
